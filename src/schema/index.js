e8900fa36826126e47c72b3d0f893e5f12d56cd6
Ben Booth <bkbooth@gmail.com>
2018-09-07 16:11:42 +1000

    Break schema into multiple stitched schemas

--- src/schema/index.js
+++ src/schema/index.js
@@ -2,33 +2,25 @@ import {
   gql,
 } from 'apollo-server-express'
 
-export default gql `
-  type Query {
-    users: [User!]
-    user(id: ID!): User
-    me: User
+import userSchema from './user'
+import messageSchema from './message'
 
-    messages: [Message!]!
-    message(id: ID!): Message!
+const linkSchema = gql `
+  type Query {
+    _: Boolean
   }
 
   type Mutation {
-    createMessage(text: String!): Message!
-    updateMessage(id: ID!, text: String!): Message!
-    deleteMessage(id: ID!): Boolean!
+    _: Boolean
   }
 
-  type User {
-    id: ID!
-    username: String!
-    firstName: String
-    age: Int
-    messages: [Message!]
-  }
-
-  type Message {
-    id: ID!
-    text: String!
-    user: User!
+  type Subscription {
+    _: Boolean
   }
 `
+
+export default [
+  linkSchema,
+  userSchema,
+  messageSchema,
+]
---
+++ src/schema/user.js
@@ -0,0 +1,19 @@
+import {
+  gql,
+} from 'apollo-server-express'
+
+export default gql `
+  extend type Query {
+    users: [User!]
+    user(id: ID!): User
+    me: User
+  }
+
+  type User {
+    id: ID!
+    username: String!
+    firstName: String
+    age: Int
+    messages: [Message!]
+  }
+`
