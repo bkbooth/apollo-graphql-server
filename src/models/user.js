2e00f06445355cedb64b9d5ef1c9917ae82f5701
Ben Booth <bkbooth@gmail.com>
2018-11-06 12:48:33 +1100

    Add some model validations and error messages

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,9 @@ const user = (sequelize, DataTypes) => {
   const User = sequelize.define('user', {
     username: {
       type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: { notEmpty: true },
     },
   })
 
