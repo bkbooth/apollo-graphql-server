89c3c050a0993666fc5883515a7bbe0883394977
Ben Booth <bkbooth@gmail.com>
2018-11-08 13:15:05 +1100

    Handle async/await in tests

--- src/tests/user.spec.js
+++ src/tests/user.spec.js
@@ -1,7 +1,14 @@
 import { expect } from 'chai'
 
 describe('users', () => {
-  it('user is user', () => {
-    expect('user').to.eql('user')
+  it('user is user', async () => {
+    const done = await delay(true)
+    expect(done).to.be.true
   })
 })
+
+function delay(result, ms = 1000) {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(result), ms)
+  })
+}
